4ed33baa7ed7f6d940388b937e06b92c92901591
Rivu Naskar <rivunaskar0@gmail.com>
2023-03-03 20:37:31 +0530

    added searchbar

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,46 @@
 import { RestaurantCard } from "./ResturantCard"
 import { restaurantList } from "../constants"
+import { useState } from "react"
+
+function filterData(restaurants, searchText) {
+    const filteredData = restaurants.filter((restaurant)=>{
+        return restaurant.data.data.name.includes(searchText);
+    });
+
+    return filteredData;
+}
+
 const Body = ()=>{
+    const [searchText, setSearchText] = useState("");
+    const [restaurants, setRestaurants] = useState(restaurantList);
     return(
+        <>
+        <div className="search-continaer">
+            <input
+            type="text"
+            className="search-input"
+            placeholder="Search"
+            value={searchText}
+            onChange={(e)=>{
+                setSearchText(e.target.value)
+            }}
+            />
+            <button onClick={()=>{
+                const data = filterData(restaurants, searchText);
+                setRestaurants(data);
+
+            }}>Search</button>
+        </div>
+
+
         <div className='restaurant-list'>
             {
-                restaurantList.map((restaurant) =>{
+                restaurants.map((restaurant) =>{
                     return <RestaurantCard {...restaurant.data.data} key={restaurant.data.data.id}/>
                 })
             }
         </div>
+        </>
     )
    
 }
